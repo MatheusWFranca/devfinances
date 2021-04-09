25d33e87075a15e1675d845ae34e91cf1cc5d51c
Matheus França <mateusanches2@gmail.com>
2021-04-09 15:05:05 -0300

    FIX

--- scripts.js
+++ scripts.js
@@ -9,11 +9,14 @@ const Modal = {
 
 const Storage = {
   get() {
-    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || [];
+    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
   },
   set(transactions) {
-    localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions));
-  }
+    localStorage.setItem(
+      'dev.finances:transactions',
+      JSON.stringify(transactions),
+    );
+  },
 };
 
 const Transaction = {
@@ -27,7 +30,7 @@ const Transaction = {
 
   remove(index) {
     Transaction.all.splice(index, 1);
-  
+
     App.reload();
   },
 
@@ -104,21 +107,21 @@ const DOM = {
   },
 
   clearTransactions() {
-    DOM.transactionsContainer.innerHTML = "";
-  }
+    DOM.transactionsContainer.innerHTML = '';
+  },
 };
 
 const Utils = {
-  formatAmount (value) {
+  formatAmount(value) {
     value = Number(value) * 100;
 
-    return value
+    return value;
   },
 
   formatDate(date) {
-    const splittedDate = date.split("-");
-    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
- },
+    const splittedDate = date.split('-');
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
 
   formatCurrency(value) {
     const signal = Number(value) < 0 ? '-' : '';
@@ -145,23 +148,24 @@ const Form = {
     return {
       description: Form.description.value,
       amount: Form.amount.value,
-      date: Form.date.value
-    }
+      date: Form.date.value,
+    };
   },
 
   validadeFields() {
     const { description, amount, date } = Form.getValues();
 
-    if(
-      description.trim() === "" ||
-      amount.trim() === "" ||
-      date.trim() === "") {
-        throw new Error("Por favor, preencha todos os campos.")
-      }
+    if (
+      description.trim() === '' ||
+      amount.trim() === '' ||
+      date.trim() === ''
+    ) {
+      throw new Error('Por favor, preencha todos os campos.');
+    }
   },
 
   saveTransaction(transaction) {
-    Transaction.add(transaction)
+    Transaction.add(transaction);
   },
 
   formatValues() {
@@ -174,14 +178,14 @@ const Form = {
     return {
       description,
       amount,
-      date
-    }
+      date,
+    };
   },
 
   clearFiels() {
-    Form.description.value = "";
-    Form.amount.value = "";
-    Form.date.value = "";
+    Form.description.value = '';
+    Form.amount.value = '';
+    Form.date.value = '';
   },
 
   submit(event) {
@@ -193,20 +197,20 @@ const Form = {
       // Formatar
       Transaction.add(transaction);
       // Salvar & add os dados do formulario
-      Form.clearFiels()
+      Form.clearFiels();
       // apagar os dados
       Modal.close();
       // Fechar modal
     } catch (error) {
       alert(error.message);
     }
-  }
+  },
 };
 
 const App = {
   init() {
-    Transaction.all.forEach(DOM.addTransaction)
-    
+    Transaction.all.forEach(DOM.addTransaction);
+
     DOM.updateBalance();
 
     Storage.set(Transaction.all);
